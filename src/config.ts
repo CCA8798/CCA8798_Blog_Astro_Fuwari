24f8f97b64b6202fc7274534461591c9c587e28f
CCA8798 <2044187229@qq.com>
2026-05-01 03:52:36 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -46,7 +46,7 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		{
 			name: "抽奖",
-			url: "https://www.cca8798.com/lotteryTimes", // Internal links should not include the base path, as it is automatically added
+			url: "https://www.cca8798.com/lotteryTimes/", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 		{
